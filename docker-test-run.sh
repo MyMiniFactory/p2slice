0c01778af1c942f04821f2217287facf2fba7b7d
tiger <ynotstartups@gmail.com>
2018-08-08 14:54:09 +0100

    easier sh for docker-test-run

--- docker-test-run.sh
+++ docker-test-run.sh
@@ -1,23 +1,13 @@
-docker run -v $PWD/p2slice_test:/app/p2slice_test -it p2slice --original_mesh_path ./p2slice_test/no-union-test.stl --pythonpath . --error_path ./p2slice_test/error.log --tmp_path ./p2slice_test/tmp --meshlab_command meshlab --enable_union
+filesToTest="
+    no-union-test.stl
+    union-test.stl
+    split-test.stl
+    tightly-arranged.stl
+    no-tightly-arranged.stl
+"
 
-python3 ./upload_process_P2Slice/validate.py p2slice_test/tmp/process_report.json
-
-docker run -v $PWD/p2slice_test:/app/p2slice_test -it p2slice --original_mesh_path ./p2slice_test/union-test.stl --pythonpath . --error_path ./p2slice_test/error.log --tmp_path ./p2slice_test/tmp --meshlab_command meshlab --enable_union
-
-python3 ./upload_process_P2Slice/validate.py p2slice_test/tmp/process_report.json
-
-docker run -v $PWD/p2slice_test:/app/p2slice_test -it p2slice --original_mesh_path ./p2slice_test/split-test.stl --pythonpath . --error_path ./p2slice_test/error.log --tmp_path ./p2slice_test/tmp --meshlab_command meshlab --enable_union
-
-python3 ./upload_process_P2Slice/validate.py p2slice_test/tmp/process_report.json
-
-docker run -v $PWD/p2slice_test:/app/p2slice_test -it p2slice --original_mesh_path ./p2slice_test/simplify-test.stl --pythonpath . --error_path ./p2slice_test/error.log --tmp_path ./p2slice_test/tmp --meshlab_command meshlab --enable_union
-
-python3 ./upload_process_P2Slice/validate.py p2slice_test/tmp/process_report.json
-
-docker run -v $PWD/p2slice_test:/app/p2slice_test -it p2slice --original_mesh_path ./p2slice_test/tightly-arranged.stl --pythonpath . --error_path ./p2slice_test/error.log --tmp_path ./p2slice_test/tmp --meshlab_command meshlab --enable_union
-
-python3 ./upload_process_P2Slice/validate.py p2slice_test/tmp/process_report.json
-
-docker run -v $PWD/p2slice_test:/app/p2slice_test -it p2slice --original_mesh_path ./p2slice_test/no-tightly-arranged.stl --pythonpath . --error_path ./p2slice_test/error.log --tmp_path ./p2slice_test/tmp --meshlab_command meshlab --enable_union
-
-python3 ./upload_process_P2Slice/validate.py p2slice_test/tmp/process_report.json
+for file in $filesToTest; do
+    echo Running on "$file" results
+    docker run --rm -v $PWD/p2slice_test:/app/p2slice_test -it p2slice --original_mesh_path ./p2slice_test/$file --pythonpath . --error_path ./p2slice_test/error.log --tmp_path ./p2slice_test/tmp --meshlab_command meshlab --enable_union >/dev/null 2>&1
+    python3 ./upload_process_P2Slice/validate.py p2slice_test/tmp/process_report.json
+done
