fa193106a065853c80ce970136286c0a7d459a18
tiger <ynotstartups@gmail.com>
2018-08-08 15:58:48 +0100

    add simplify file

--- docker-test-run.sh
+++ docker-test-run.sh
@@ -5,10 +5,11 @@ filesToTest="
     tightly-arranged.stl
     tightly-arranged-1.stl
     no-tightly-arranged-1.stl
+    simplify-test.stl
 "
 
 for file in $filesToTest; do
     echo Running on "$file" results
-    docker run --rm -v $PWD/p2slice_test:/app/p2slice_test -it p2slice --original_mesh_path ./p2slice_test/$file --pythonpath . --error_path ./p2slice_test/error.log --tmp_path ./p2slice_test/tmp --meshlab_command meshlab --enable_union >/dev/null 2>&1
+    docker run --rm -v $PWD/p2slice_test:/app/p2slice_test -it p2slice --original_mesh_path ./p2slice_test/$file --pythonpath . --error_path ./p2slice_test/error.log --tmp_path ./p2slice_test/tmp --meshlab_command meshlab --enable_union --verbose >/dev/null 2>&1
     python3 ./upload_process_P2Slice/validate.py p2slice_test/tmp/process_report.json
 done
